3c136826ca7e84c9c4fd7f8cc86c573decbe60e4
Tavares Neto <tavares.agtn@gmail.com>
2021-11-09 22:19:16 -0300

    Commit Desafio
    Commit de desafio com resolução e arquivos gerados.

--- PyFinanceiro/main.py
+++ PyFinanceiro/main.py
@@ -1,1 +1,88 @@
+def file_to_data(file, array):
+    
+    file.readline()
+    
+    for x in file:
+        array.append(x.rstrip())
+    
+    return array
 
+
+def data_sort(firstArray, secondArray, originArray):
+    
+    for x in originArray:
+        data, valor = x.split(',')
+        firstArray.append(data)
+        secondArray.append(int(valor))
+    return firstArray, secondArray
+
+
+def variance_min_max(valores):
+    
+    variance = []
+    for x in range(1, len(valores)):
+        variance.append(valores[x] - valores[x - 1])
+    return variance
+
+
+def formato(datas):
+    
+    mes_longo = ['Janeiro de ', 'Fevereiro de ', 'Março de ', 'Abril de ', 'Maio de ', 'Junho de ',
+    'Julho de ', 'Agosto de ', 'Setembro de ', 'Outubro de ', 'Novembro de ', 'Dezembro de ']
+    mes_curto = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+    mes = ano = ''
+    datas_longo = []
+
+    for x in datas:
+        mes, ano = x.split('-')
+        for y in range(0, len(mes_curto)):
+            if mes == mes_curto[y]:
+                mes = mes_longo[y]
+        data_completa = mes + ano
+        datas_longo.append(data_completa)
+        pass
+
+    return datas_longo
+
+
+def report(datas, valores, variancia):
+    
+    total_meses = (len(datas))
+    valor_liquido = sum(int(x) for x in valores)
+    variancia_media = (sum(variancia)/(total_meses-1))
+    indice_maior_lucro = variancia.index(max(variancia))
+    indice_maior_perda = variancia.index(min(variancia))
+
+    texto_relatorio = ('Análise Financeira\n'
+    +'-----------------------------------------------------------\n'
+    +f'Total de meses:\t\t\t{total_meses} meses\n'
+    +f'Total arrecadado:\t\t$ {valor_liquido}\n'
+    +f'Variação da média:\t\t$ {round(variancia_media, 2)}\n'
+    +f'Maior aumento nos lucros:\t{datas[indice_maior_lucro+1]} ($ {max(variancia)})\n'
+    +f'Maior redução nos lucros:\t{datas[indice_maior_perda+1]} ($ {min(variancia)})\n')
+
+    return texto_relatorio
+
+
+def main():
+    
+    matriz = []
+    datas = []
+    valores = []
+
+    with open('PyFinanceiro\Recursos\dados_financeiro.txt', 'r') as dados:
+        matriz = file_to_data(dados, matriz)
+    
+    datas, valores = (data_sort(datas, valores, matriz))
+    variancia = variance_min_max(valores)
+    datas = formato(datas)
+    texto_relatorio = report(datas, valores, variancia)
+
+    with open('relatório.txt', 'w') as relatorio:
+        relatorio.write(texto_relatorio)
+
+    print(texto_relatorio)
+
+
+if __name__ == '__main__':
+    main()
--- PyVotacao/main.py
+++ PyVotacao/main.py
@@ -1,1 +1,45 @@
+def main():
+    
+    ballot = []
+    candidates = {
+        "Khan": 0,
+        "Correy": 0,
+        "Li": 0,
+        "OTooley": 0
+        }
 
+    with open('PyVotacao\Recursos\dados_elecao.txt', 'r') as urna:
+        for x in urna:
+            trash, trash2, voto = x.split(',')
+            ballot.append(voto.rstrip())
+        del(ballot[0])
+    
+    for x in ballot:
+        if x == "Khan": candidates["Khan"] += 1
+        if x == "Correy": candidates["Correy"] += 1
+        if x == "Li": candidates["Li"] += 1
+        if x == "O'Tooley": candidates["OTooley"] += 1
+
+    OTooley = "O'Tooley"
+    Vencedor = max(candidates, key=candidates.get)
+
+    texto_resultado = ('Resultados Eleitorais\n'
+    +'------------------------\n'
+    +f'Total de votos: {len(ballot)}\n'
+    +'------------------------\n'
+    +f'Khan: {round((100/len(ballot)) * candidates["Khan"], 4)}% ({candidates["Khan"]})\n'
+    +f'Correy: {round((100/len(ballot)) * candidates["Correy"], 4)}% ({candidates["Correy"]})\n'
+    +f'Li: {round((100/len(ballot)) * candidates["Li"], 4)}% ({candidates["Li"]})\n'
+    +f'{OTooley}: {round((100/len(ballot)) * candidates["OTooley"], 4)}% ({candidates["OTooley"]})\n'
+    +'------------------------\n'
+    +f'Vencedor: {Vencedor}\n'
+    +'------------------------')
+
+    with open('Resultado.txt', 'w') as resultado:
+        resultado.write(texto_resultado)
+
+    print(texto_resultado)
+
+
+if __name__ == '__main__':
+    main()
